bcd172da88e79fdedc49f9cde5e0a4b92f0621be
Danny-ell <danielolawale06@gmail.com>
2022-09-14 16:08:32 -0700

    Ham burgers

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -17,4 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* MAIN_H */
