e4004e61e5bf7d00183f04cd685d5ab0809c6163
Danny-ell <danielolawale06@gmail.com>
2022-09-15 18:21:30 -0700

    Living the life

--- 0x04-more_functions_nested_loop/main.h
+++ 0x04-more_functions_nested_loop/main.h
@@ -1,6 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,4 +11,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
