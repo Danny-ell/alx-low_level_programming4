35d41d080a51801cf08c97ad256afa940f46d846
Danny-ell <danielolawale06@gmail.com>
2022-09-22 18:04:13 -0700

    expectation

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
 /**
- * string_toupper - function to transform all lowercase in string to uppercase
- * 
+ * cap_string - capitalizes chars in a string following a separator
+ *
  * @c: character string pointer
  * Return: char pointer
  */
-char *string_toupper(char *c)
+char *cap_string(char *c)
 {
-	int i = 0;
+	int i = 0, j,
+	sep[] = {32, '\t', 11,  '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (c[0] > 96 && c[0] < 123)
+		c[0] -= 32;
 	while (c[i] != '\0')
 	{
 		if (c[i] > 96 && c[i] < 123)
-			c[i] -= 32;
+		{
+			j = 0;
+			while (j < 14)
+			{
+				if (c[i - 1] == sep[j])
+				{
+					c[i] -= 32;
+					break;
+				}
+				j++;
+			}
+		}
 		i++;
 	}
 	return (c);
 }
+
